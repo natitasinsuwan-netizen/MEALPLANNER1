958737b8bde217aab4f1aaea6425eb9b5ce0db3f
natita <natitasinsuwan@gmail.com>
2026-09-18 19:09:22 +0700

    Admin add: Created new dish "Katsudon" (900 kcal)

--- meals.js
+++ meals.js
@@ -1,5 +1,34 @@
 const DEFAULT_MEALS = [
   {
+    "id": 1789733359120,
+    "name": "Katsudon",
+    "description": "deep fried pork cutlet with egg rice bowl",
+    "calories": 900,
+    "protein_g": 35,
+    "carbs_g": 110,
+    "fat_g": 35,
+    "low_fat": false,
+    "image_url": "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?auto=format&fit=crop&w=600&q=80",
+    "keywords": {
+      "countries": [
+        "Japanese"
+      ],
+      "cooking_methods": [
+        "Stir-Fry"
+      ],
+      "carbs": [
+        "Rice"
+      ],
+      "protein": [
+        "Pork"
+      ]
+    },
+    "allergens": [
+      "egg"
+    ],
+    "dietary_tags": []
+  },
+  {
     "id": 1789733191189,
     "name": "Gyudon",
     "description": "Beef with sweet soy sauce and onion with rice",
